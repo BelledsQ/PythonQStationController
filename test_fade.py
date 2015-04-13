2bb78f475c8ee95086d47c218759cd970964cb9b
mDan T <dmtaub@gmail.com>
2015-04-13 07:45:56 -0400

    more functions in test_fade.py

--- test_fade.py
+++ test_fade.py
@@ -14,35 +14,44 @@ lights=udpClient.get_lights()
 sns = []
 for light in lights['led']:
     sns.append(light['sn'])
+    print "found light: "+light['sn']
 
 def send(r,g,b,bright=DEFAULT_BRIGHT):
     udpClient.set_light(bright,r,g,b,ONLINE,sns)
 
-r=0
-g=0
-b=0
-colormax = 1
-step = 2.0/256
-delay = 0.01
+def main():
+    #fadeloop()
+    send(255,145,15)
 
-while r < colormax :
-    send(int(r*255),int(g*255),int(b*255))
-    r=r+step
-    time.sleep(delay)
-    
-while True:
-    while g < colormax :
-        send(int(r*255),int(g*255),int(b*255))
-        r=r-step
-        g=g+step
-        time.sleep(delay)
-    while b < colormax :
-        send(int(r*255),int(g*255),int(b*255))
-        g=g-step
-        b=b+step
-        time.sleep(delay)
+def fadeloop():
+    r=0
+    g=0
+    b=0
+    colormax = 1
+    step = 2.0/256
+    delay = 0.01
+
     while r < colormax :
         send(int(r*255),int(g*255),int(b*255))
-        b=b-step
         r=r+step
         time.sleep(delay)
+        
+    while True:
+        while g < colormax :
+            send(int(r*255),int(g*255),int(b*255))
+            r=r-step
+            g=g+step
+            time.sleep(delay)
+        while b < colormax :
+            send(int(r*255),int(g*255),int(b*255))
+            g=g-step
+            b=b+step
+            time.sleep(delay)
+        while r < colormax :
+            send(int(r*255),int(g*255),int(b*255))
+            b=b-step
+            r=r+step
+            time.sleep(delay)
+
+if __name__ == "__main__":
+    main()
